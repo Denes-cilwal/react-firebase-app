17015fe0e699ed54421b2a7305c019d71c5efd18
Denes-cilwal <dinesh.silwal@wesionary.team>
2021-12-05 09:53:16 +0545

    usefirebase func to fetch data with id :

--- firebase-todo-app/src/App.js
+++ firebase-todo-app/src/App.js
@@ -1,14 +1,37 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import { AddButtonComponent } from "./Components/atoms/addButton";
 import { FormComponent } from "./Components/atoms/Form";
 import { Todo } from "./Components/todo";
-import { Typography } from '@material-ui/core';
+import { Typography } from "@material-ui/core";
+import { db } from "./Config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import "./App.css";
+
 
 function App() {
-  //setting shorttime memory for react-state , it clears when page refreshes, since it is not save to databases
-  const [todos, setTodos] = useState(["Starting ...", "Adding Data ... "]);
+  //setting shorttime memory for react-state , 
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
+
+  const todosCollectionRef = collection(db, "todos");
+  console.log(todosCollectionRef, "abcd")
+
+  useEffect(() => {
+    // gets called when app loads | page renders
+    const getTodos = async () => {
+
+      // getDocs return all documents from specfic collections
+      const data = await getDocs(todosCollectionRef)
+      // fetching data with id 
+      setTodos(data.docs.map(doc => ([doc.id ,"----",doc.data().todos])))
+    }
+    getTodos()
+  }, [])
+
+
+
+
+
 
   const addTodosHandler = (event) => {
     //prevant from refreshing page on adding todos
@@ -19,24 +42,19 @@ function App() {
     // clear input field after adding todos in the list
     setInput("");
   };
-  
-
 
   return (
     <div className="App">
       <Typography>React-App -- 🤖 </Typography>
-      <FormComponent
-      input={input}
-      setInput={setInput}
-      />
+      <FormComponent input={input} setInput={setInput} />
 
-      <AddButtonComponent addTodosHandler={addTodosHandler} input={input} />
       <ul>
         {/* map array todos with every element todo */}
         {todos.map((todo) => (
           <Todo todo={todo} key={todo.id} />
         ))}
       </ul>
+      <AddButtonComponent addTodosHandler={addTodosHandler} input={input} />
     </div>
   );
 }
--- firebase-todo-app/src/Config/Firebase.js
+++ firebase-todo-app/src/Config/Firebase.js
@@ -1,12 +1,13 @@
-import firebase from "firebase/app";
-
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore';
 import {
   FIREBASE_API_KEY,
   FIREBASE_APP_ID,
   FIREBASE_AUTH_DOMAIN,
   FIREBASE_MESSAGING_SENDER_ID,
   FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUCKET
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MEASUREMENT_ID,
 } from "../constants";
 
 const firebaseConfig = {
@@ -16,10 +17,12 @@ const firebaseConfig = {
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
   appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+// initialize firebase connection with firebase config...
+const app = initializeApp(firebaseConfig);
 
-const db = firebaseApp.firestore();
 
-export { db };
+export const db = getFirestore(app);
+
